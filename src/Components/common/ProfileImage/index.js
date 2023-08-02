db51b042f243316d67dc3541ed82b4969aa63880
Vrushali Murlidhar Patil <vrushalimpatil2001@gmil.com>
2023-08-02 15:14:11 +0530

    first commit

---
+++ src/Components/common/ProfileImage/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function ProfileImage(profileImage) {
+
+    return (
+        <div>
+            <Link to={`/profile/`}>
+                <div className="profile-img">
+                    <img className="image" style={{justifyContent:"center"}} src={profileImage} alt=""/>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default ProfileImage
+
